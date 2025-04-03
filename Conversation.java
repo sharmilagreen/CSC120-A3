47f855cbe5d6a584fddf0f4fe677c2014841c909
sharmilagreen <sagreen@smith.edu>
2025-04-02 20:53:07 -0400

    Wrote basic code for random response conversation, did not fill out respond method.

--- Conversation.java
+++ Conversation.java
@@ -1,27 +1,46 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Random;
 
 class Conversation implements Chatbot {
 
   // Attributes 
-
+  private ArrayList transcript;
+  private ArrayList convoOptions = new ArrayList<>();
+  Random random = new Random();
   /**
    * Constructor 
    */
   Conversation() {
-    
+    transcript = new ArrayList();
+    convoOptions.add("Huh...");
+    convoOptions.add("Interesting.");
+    convoOptions.add("Cool!");
   }
 
   /**
    * Starts and runs the conversation with the user
    */
   public void chat() {
-
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("How many rounds would you like?");
+    int rounds = scanner.nextInt();
+    System.out.println("Hello! What's going on?");
+    String input = scanner.nextLine();
+    for(int i = 0; i < rounds; i++){
+      int index = random.nextInt(3);
+      input = scanner.nextLine();
+      System.out.println(convoOptions.get(index));
+      transcript.add(input);
+    }
+    System.out.println("Goodbye!");
   }
 
   /**
    * Prints transcript of conversation
    */
   public void printTranscript() {
-
+    System.out.println(transcript);
   }
 
   /**
